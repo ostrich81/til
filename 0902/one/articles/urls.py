bf7c835c89f14142012cb33800ca5f554754a5ed
unknown <djr53rt@gmail.com>
2021-09-03 14:06:48 +0900

    0903

---
+++ 0902/one/articles/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+from . import views
+
+
+app_name = 'articles'
+
+urlpatterns = [
+    path('index/',views.index,name='index'),
+    path('new/',views.new,name='new'),
+    path('create/',views.create,name='create'),
+    path('<int:pk>/',views.detail,name='detail'), #http://127.0.0.1:8000/articles/1 1번 게시글 상세가 보이도록 
+    path('<int:pk>/delete/',views.delete,name='delete'),
+    path('<int:pk>/edit/',views.edit,name='edit'),
+    path('<int:pk>/update/',views.edit,name='update'),
+    
+]
