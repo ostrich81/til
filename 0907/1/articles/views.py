1142e7bdbc52af8e1553c12f21a809191022bae2
unknown <djr53rt@gmail.com>
2021-09-08 12:50:31 +0900

    0908

--- 0907/1/articles/views.py
+++ 0907/1/articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
-from .models import Article 
 from .forms import ArticleForm
+from .models import Article 
 # Create your views here.
 def index(request):
     articles=Article.objects.all()
